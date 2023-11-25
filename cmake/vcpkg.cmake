b22df81e90aa74982d329aa3eaa4fd6669926dbf
Andy Fillebrown <andy@docEdub.com>
2023-11-25 04:40:38 -0500

    Fetch vcpkg release with url instead of git tag

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -6,8 +6,10 @@ message(STATUS "Fetching vcpkg")
 include(FetchContent)
 FetchContent_Declare(
     vcpkg
-    GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
-    GIT_TAG 2023.11.20
+    URL https://github.com/microsoft/vcpkg/archive/refs/tags/2023.11.20.tar.gz
+    URL_MD5 24d44e89fc845d67051a52ed5066fc84
+    DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/vcpkg"
+    DOWNLOAD_EXTRACT_TIMESTAMP true
 )
 FetchContent_MakeAvailable(vcpkg)
 message(STATUS "Fetching vcpkg - done")
